10c8d63757c5105a827bdb18a3b28592e800de95
engMaya22 <eng.maya.esmaeel1@gmail.com>
2024-12-01 05:03:08 -0800

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,32 @@
-
+import aboutImage  from   "../assets/about.jpg"
+import {ABOUT_TEXT} from "../constants"
 const About =()=>{
     return <div className="pb-4 border-b border-neutral-900">
              <h1 className="my-20 text-center text-4xl">
-                About Me
+                About <span className="text-neutral-500">
+                Me
+                </span>
              </h1>
+             <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2 lg:p-8">
+
+                  <div className="flex items-center justify-center">
+                    <img src={aboutImage} alt="about" className="rounded-2xl"/>
+                  </div>
+
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+
+                    <div className="flex  justify-center lg:justify-start">
+                        <p className=" my-2 max-w-xl py-6"> {ABOUT_TEXT} </p>
+                         
+                    </div>
+
+                </div>
+
+
+
+             </div>
 
           </div>
 
